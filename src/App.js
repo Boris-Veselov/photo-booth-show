1dee438d6086c117887d5d146adc8262c0878131
Boris Veselov <borisveselov@yahoo.com>
2022-06-10 21:30:32 -0400

    updated components

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ function App() {
       <main>
         {!contactSelected ? (
           <>
-            <Gallery currentCategory={currentCategory}></Gallery>
             <About></About>
+            <Gallery currentCategory={currentCategory}></Gallery>
           </>
         ) : (
             <ContactForm></ContactForm>
